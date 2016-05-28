e483cc6742c2cca6dbc72de0af2fa10c7d054ba7
Jason Shao <jasonshao8@gmail.com>
2016-05-27 22:09:03 -0400

    bad seg to coord parsing

--- parse.py
+++ parse.py
@@ -7,8 +7,9 @@ all_data = json.load(roads_file)
 graph = {}
 dist = {}
 coord_to_seg = {}
+seg_to_coord = {}
 
-black_list = set([26896, 1517, 7608, 1526, 7947, 7494, 7469, 9064, 9062, 7572, 7514, 259, 1621, 29101, 30205, 7289, 274, 348])
+black_list = set([26896, 1517, 7608, 1526, 7947, 7494, 7469, 9064, 9062, 7572, 7514, 259, 1621, 29101, 30205, 7289, 274, 348, 80267])
 
 for road in all_data['features']:
     if road['properties']['SEGMENT_ID'] in black_list:
@@ -20,9 +21,10 @@ for road in all_data['features']:
     if road['geometry']['type'] == 'LineString':
         coord_to_seg[(from_coord, to_coord)] = road['properties']['SEGMENT_ID']
         coord_to_seg[(to_coord, from_coord)] = road['properties']['SEGMENT_ID']
+        seg_to_coord[road['properties']['SEGMENT_ID']] = road['geometry']['coordinates']
         if road['properties']['FLOW_DIR'] == 'TwoWay':
-            if from_coord not in graph:
-                graph[from_coord] = []
+            if from_coord not in graph: 
+                graph[from_coord] = [] 
             graph[from_coord].append(to_coord)
             dist[(from_coord, to_coord)] = road['properties']['LENGTH_M']
             dist[(to_coord, from_coord)] = road['properties']['LENGTH_M']
@@ -40,13 +42,34 @@ for road in all_data['features']:
             graph[from_coord].append(to_coord)
             dist[(from_coord, to_coord)] = road['properties']['LENGTH_M']
 
-start = (-80.5328174269999, 43.4732988380001)
-end = (-80.5002613569999,43.511091493)
-for (a, b) in astar(start, end, graph, dist):
-    print(b, ',', a)
+input_list = [7294, 274, 389]
+if len(input_list) == 1:
+    start = seg_to_coord[input_list[0]][0]
+    end = seg_to_coord[input_list[-1]][-1]
+else:
+    if seg_to_coord[input_list[0]][0] in seg_to_coord[input_list[1]]:
+        start = seg_to_coord[input_list[0]][-1]
+    else:
+        start = seg_to_coord[input_list[0]][0]
 
-#optimal_path = list(reversed(astar(start, end, graph, dist)))
-#seg_path = []
-#for i in range(1, len(optimal_path)):
-#    seg_path.append(coord_to_seg[(optimal_path[i-1], optimal_path[i])])
-#print seg_path
+    if seg_to_coord[input_list[-1]][0] in seg_to_coord[input_list[-2]]:
+        end = seg_to_coord[input_list[-1]][-1]
+    else:
+        end = seg_to_coord[input_list[-1]][0]
+
+start = tuple(start)
+end = tuple(end)
+
+print start, end
+print seg_to_coord[input_list[-1]]
+#start = (-80.5328174269999, 43.4732988380001)
+#end = (-80.5002613569999,43.511091493)
+
+optimal_path = list(reversed(astar(start, end, graph, dist)))
+optimal_coords = []
+for i in range(1, len(optimal_path)):
+    seg = coord_to_seg[(optimal_path[i-1], optimal_path[i])]
+    optimal_coords.extend(seg_to_coord[seg])
+
+for i in optimal_coords:
+    print i[1], ',', i[0]
