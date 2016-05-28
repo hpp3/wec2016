d8c722a174677814a5988aefac2c08453cba7cda
Jason Shao <jasonshao8@gmail.com>
2016-05-27 21:33:57 -0400

    Add blacklist

--- parse.py
+++ parse.py
@@ -1,45 +1,25 @@
 import json
 from astar import astar
 
-def dijkstra(graph, edges, initial):
-  visited = {initial: 0}
-  path = {}
-
-  nodes = set(graph)
-
-  while nodes: 
-    min_node = None
-    for node in nodes:
-      if node in visited:
-        if min_node is None:
-          min_node = node
-        elif visited[node] < visited[min_node]:
-          min_node = node
-
-    if min_node is None:
-      break
-
-    nodes.remove(min_node)
-    current_weight = visited[min_node]
-
-    for edge in graph.edges[min_node]:
-      weight = current_weight + graph.distance[(min_node, edge)]
-      if edge not in visited or weight < visited[edge]:
-        visited[edge] = weight
-        path[edge] = min_node
-
-  return visited, path
-
 roads_file = open('roads.json', 'r')
+
 all_data = json.load(roads_file)
 graph = {}
 dist = {}
+coord_to_seg = {}
+
+black_list = set([26896, 1517, 7608, 1526, 7947, 7494, 7469, 9064, 9062, 7572, 7514, 259, 1621, 29101, 30205, 7289, 274, 348])
 
 for road in all_data['features']:
+    if road['properties']['SEGMENT_ID'] in black_list:
+        continue
+
     from_coord = tuple(road['geometry']['coordinates'][0])
     to_coord = tuple(road['geometry']['coordinates'][-1])
 
     if road['geometry']['type'] == 'LineString':
+        coord_to_seg[(from_coord, to_coord)] = road['properties']['SEGMENT_ID']
+        coord_to_seg[(to_coord, from_coord)] = road['properties']['SEGMENT_ID']
         if road['properties']['FLOW_DIR'] == 'TwoWay':
             if from_coord not in graph:
                 graph[from_coord] = []
@@ -65,3 +45,8 @@ end = (-80.5002613569999,43.511091493)
 for (a, b) in astar(start, end, graph, dist):
     print(b, ',', a)
 
+#optimal_path = list(reversed(astar(start, end, graph, dist)))
+#seg_path = []
+#for i in range(1, len(optimal_path)):
+#    seg_path.append(coord_to_seg[(optimal_path[i-1], optimal_path[i])])
+#print seg_path
